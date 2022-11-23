3b54c1fd7e5c8fe9f26565fdfcf2ed8f4b742ac4
Arty <arty-laz@mail.ru>
2022-11-23 22:34:45 +0500

    Начал создавать интерфейс сайта
    - Написал шапку сайта
    - Создал структуру файлов
    - Добавил не обходимые библиотеки

---
+++ App/493907_LazarishinArtur_opt/493907_LazarishinArtur_opt/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _493907_LazarishinArtur_opt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
